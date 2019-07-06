3ae762c0102643cdfc59e4085865a9509c1a8935
komsic <kayodemicheal159@gmail.com>
2019-07-06 20:10:47 +0100

    chore(refactor-pr8): remove duplicate code [Finishes #167117603]

--- test/test.js
+++ test/test.js
@@ -18,17 +18,3 @@ describe('Test GET / Status Codes', () => {
     expect(res).to.have.status(404);
   });
 });
-
-describe('Test GET / Versioning', () => {
-  it('should have a 200 status code', async () => {
-    const res = await chai.request(app).get('/api');
-
-    expect(res).to.have.status(200);
-  });
-
-  it('should have a 404 status code', async () => {
-    const res = await chai.request(app).get('/api/v1');
-
-    expect(res).to.have.status(200);
-  });
-});
