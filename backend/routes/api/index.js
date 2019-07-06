d615bb1394c946dee75dbdc220ed0a2f0652451f
komsic <kayodemicheal159@gmail.com>
2019-07-06 20:10:47 +0100

    chore(refactor-pr8): remove duplicate code [Finishes #167117603]

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,7 @@
 import express from 'express';
 import v1 from './v1';
-import REDIRECT_MESSAGE from '../../utils/constants';
 
 const router = express.Router();
-
-router.get('/', (_req, res) => {
-  res.status(200).send(`You've reached the API Routes. ${REDIRECT_MESSAGE}`);
-});
 
 router.use('/v1', v1);
 
