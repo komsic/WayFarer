8a5cf21d93a8af52e9a74bfb775131366ec7239f
komsic <kayodemicheal159@gmail.com>
2019-07-09 14:18:09 +0100

    chore(database): rearrange migration code

--- backend/db/migrations/unmigrate.js
+++ backend/db/migrations/unmigrate.js
@@ -1,18 +1,14 @@
 /* eslint-disable no-console */
-import db from '../index';
 import users from './1562483808518-create-user';
 import bus from './1562493931802-create-bus';
 import trips from './1562494809787-create-trip';
 import seats from './1562495081137-create-seat';
 import bookings from './1562495343430-create-booking';
+import doMigrationOp from './op';
 
 const undoMigration = async () => {
   try {
-    await db.query(bookings.down());
-    await db.query(seats.down());
-    await db.query(trips.down());
-    await db.query(bus.down());
-    await db.query(users.down());
+    await doMigrationOp('down', bookings, seats, trips, bus, users);
     console.log('Undo Migration Done');
   } catch (error) {
     console.log(`Undo Migration Failed: ${error}`);
